7c4d9b49d1191b3b766921d9de65ab42de206923
Zahny1 <Joshuakryjom@web.de>
2025-01-17 19:30:04 +0100

    Design

--- Assets/_Scripts/MainMenuScript.cs
+++ Assets/_Scripts/MainMenuScript.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-
 public class MainMenuScript : MonoBehaviour {
-    [SerializeField] GameObject settingsMenuObj;
+    
     [SerializeField] GameObject mainMenuObj;
-    [SerializeField] GameObject overallSettingsObj;
+        [SerializeField] GameObject singlePlayerObj;
+        [SerializeField] GameObject multiPlayerObj;
+        [SerializeField] GameObject endGameObj;
+        [SerializeField] Button buttonObj;
+        [SerializeField] GameObject settingsMenuObj;
+            [SerializeField] GameObject audioSettingsObj;
+            [SerializeField] GameObject overallSettingsObj;
+            [SerializeField] GameObject videoSettingsObj;
+            [SerializeField] GameObject keybindsSettingsObj;
+
+
+    private Outline focusOutline;
+    private Color focusOutlineColor = Color.white;
+    private Color noFocusColor = Color.clear;
 
     void Start() {
         mainMenuObj.SetActive(true);
+
+        focusOutline = buttonObj.GetComponent<Outline>();
+        if (focusOutline == null) {
+            focusOutline = buttonObj.gameObject.AddComponent<Outline>();
+        }
+
+        focusOutline.effectColor = noFocusColor;
+        focusOutline.effectDistance = new Vector2(2, 2);
     }
+
+    private void Update() {
+        if (EventSystem.current.currentSelectedGameObject == buttonObj) {
+            focusOutline.effectColor = focusOutlineColor;
+        } else {
+            focusOutline.effectColor = noFocusColor;
+        }
+    }
+
     public void OpenSettingsMenu() {
         if (settingsMenuObj.activeSelf) {
             settingsMenuObj.SetActive(false);
@@ -18,13 +49,12 @@ public class MainMenuScript : MonoBehaviour {
             settingsMenuObj.SetActive(true);
         }
     }
+    
     public void OpenOverallSettings() {
         if (overallSettingsObj.activeSelf) {
             overallSettingsObj.SetActive(false);
-            settingsMenuObj.SetActive(true);
         } else {
             overallSettingsObj.SetActive(true);
-            settingsMenuObj.SetActive(false);
         }
     }
     public void LoadScene() {
